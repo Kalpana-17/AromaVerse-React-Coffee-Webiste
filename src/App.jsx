f63c45b82faa4aa042dfa1f7ef9d8095cf7c01e7
Kalpana-17 <kyamak2003@gmail.com>
2026-07-01 09:29:31 +0530

    Prepare site for GitHubPages: set base and router basename, fix deploy script

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/AromaVerse-React-Coffee-Webiste">
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/store" element={<Store />}/>
